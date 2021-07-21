13d4cdebec8fae1e3e91cef2a78a16c65e7c65d4
ASMITA <ASMITA@GMAIL.COM>
2021-07-21 12:22:49 +0530

    HashSet

--- src/SetsProg/HashSetProgram.java
+++ src/SetsProg/HashSetProgram.java
@@ -1,18 +1,29 @@
 package SetsProg;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class HashSetProgram {
 
 	public static void main(String[] args) {
+		//creating the HashSet 
 		HashSet<String> hs = new HashSet<String>();
+		
+		//Adding the element in the HashSet
 		hs.add("Z");
 		hs.add("A");
 		hs.add("B");
 		hs.add("C");
 		hs.add("C");
-		//heh
+		
 		System.out.println("Added Element in the hs : "+hs);
+		
+		//iterators over the HashSet using Iterator
+		Iterator<String> i = hs.iterator();
+		while(i.hasNext())
+		{
+			System.out.println("Element : "+i.next());
+		}
 
 	}
 
